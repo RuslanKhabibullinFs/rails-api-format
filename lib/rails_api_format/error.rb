eb6d06a14e81af376f05696676aff8e14b5c42b5
RuslanKhabibullinFs <ruslan.khabibullin@flatstack.com>
2017-03-27 11:17:25 +0300

    serializer fix

--- lib/rails_api_format/error.rb
+++ lib/rails_api_format/error.rb
@@ -19,7 +19,7 @@ module RailsApiFormat
     def to_json(options = {})
       # Specify root element as `error` instead of auto generated `rails_api_format/error`
       # https://github.com/rails-api/active_model_serializers/pull/1029#issuecomment-131896282
-      ActiveModel::SerializableResource.serialize(self, options.merge(root: :error)).to_json
+      ActiveModel::SerializableResource.new(self, options.merge(root: :error)).to_json
     end
 
     def status
